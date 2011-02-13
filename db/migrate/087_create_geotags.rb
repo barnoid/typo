b441b9c1e9f96fec34e5ba5fd3b737225f9a9005
Barney Livingston <barney@barnoid.org.uk>
2011-02-13 14:40:07 +0000

    Rename geotags migration file to make it work with db update code. Also indent cleanup.

--- db/migrate/087_create_geotags.rb
+++ db/migrate/087_create_geotags.rb
@@ -1,10 +1,10 @@
 class CreateGeotags < ActiveRecord::Migration
   def self.up
     create_table :geotags do |t|
-	  t.integer :article_id
+      t.integer :article_id
       t.float :lat, :limit => 25
       t.float :lon, :limit => 25
-	  t.integer :accuracy
+      t.integer :accuracy
       t.string :name
     end
   end
